58a47d9afcb083d61d0e970c43835fecca247767
Ryan Kosta <ryanpkosta@gmail.com>
2020-07-07 16:16:03 -0400

    changes buttoninterrupttest to toggle led

--- Src/generic_button_driver_test.c
+++ Src/generic_button_driver_test.c
@@ -5,13 +5,15 @@
  *      Author: rkosta
  */
 #include "generic_button_driver_test.h"
+
 uint8_t buttoninterrupttest(const uint8_t button)
 {
+	ledinit(LED_BLUELED);
 	buttoninterruptinit(button);
+	return(0);
 }
 void EXTI0_IRQHandler(void)
 {
-	ledinit(LED_BLUELED);
-	ledon(LED_BLUELED);
+	ledtoggle(LED_BLUELED);
 	GPIO_IRQHandle(0);
 }
