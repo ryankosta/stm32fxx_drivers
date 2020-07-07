e20999e91d2996a70a10a91624bd8927131c09fc
Ryan Kosta <ryanpkosta@gmail.com>
2020-07-07 14:51:42 -0400

    Implements button interrupttest function

--- Src/generic_button_driver_test.c
+++ Src/generic_button_driver_test.c
@@ -4,4 +4,7 @@
  *  Created on: Jul 7, 2020
  *      Author: rkosta
  */
-
+#include "generic_button_driver_test.h"
+uint8_t buttoninterrupttest(const uint8_t button){
+	buttoninterruptinit(button);
+}
